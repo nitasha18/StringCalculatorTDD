c9136008e9ee74643d4bd2b8e530118851730d74
nitasha18 <nitashag18@gmail.com>
2022-03-13 21:06:10 +0530

    check support for multiple numbers as input

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,3 +12,7 @@ def test_stringCalculator_should_return_same_value_on_single_input():
 def test_stringCalculator_return_sumation_on_two_value():
     result = add("5,4")
     assert result == 9, "String calculater should return correct addition for two numbers"
+
+def test_stringCalculator_return_sumation_for_multiple_numbers():
+    result = add("5,4,3,2,1")
+    assert result == 15, "String calculater should return correct addition for multiple numbers"
