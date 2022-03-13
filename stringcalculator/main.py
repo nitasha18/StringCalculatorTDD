7ba38d17d142332fe7732e7be5744d1801d603d6
nitasha18 <nitashag18@gmail.com>
2022-03-13 21:53:04 +0530

    ignores the numbers greater than 1000

--- stringcalculator/main.py
+++ stringcalculator/main.py
@@ -14,4 +14,6 @@ def add(numbers):
     numbers = list(map(int, re.findall(r"-?\d+", numbers)))
     if any(num < 0 for num in numbers):
         raise ValueError("Negatives are not allowed " + str([num for num in numbers if num < 0]))
+
+    numbers = list(filter(lambda x : x<1000, numbers))
     return sum(numbers)
