2e3c266244ddf0603cf2fd1847222cc33ccc7c33
nitasha18 <nitashag18@gmail.com>
2022-03-13 20:58:55 +0530

    first test case compile failure

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 import pytest
+from stringcalculator.main import add
 
-def test_nothing():
-    pass
+def test_stringCalculator_should_return_zero_on_empty_string():
+    result = add("")
+    assert result == 0, "String calculater should return 0 on empty string"
